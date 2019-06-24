e19751e87b10a626ea7531e0b026dc71d812023e
MatthewDodi <matthew.dodi@gmail.com>
2019-06-23 19:25:06 -0700

    Added new graphql custom hook

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  plugins: ["gatsby-plugin-emotion"]
+  siteMetadata: {
+    title: "Frontend Masters Gatsby Workshop",
+    description:
+      "A site built while following the Gatsby workshop on Frontend Masters"
+  },
+  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-react-helmet"]
 };
