ff1afcebd38ac22bd42c9c4c38adb0a845d4f974
MatthewDodi <matthew.dodi@gmail.com>
2019-06-23 17:10:28 -0700

    Added new about page

---
+++ .cache/sync-requires.js
@@ -0,0 +1,12 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/matthew/Documents/All-Projects/gatsby-frontEndMasters/.cache/dev-404-page.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/matthew/Documents/All-Projects/gatsby-frontEndMasters/src/pages/index.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("/Users/matthew/Documents/All-Projects/gatsby-frontEndMasters/src/pages/about.js")))
+}
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
-import React from 'react';
+import React from "react";
+import { Link } from "gatsby";
 
-export default () => <div>Hello Frontend Masters!</div>;
+export default () => (
+  <>
+    <h1>Home</h1>
+    <p>Hello Gatsby</p>
+    <Link to="/about">About</Link>
+  </>
+);
