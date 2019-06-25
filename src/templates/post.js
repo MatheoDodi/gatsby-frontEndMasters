e63b4cc910fe8440bb9fc220040b2872001252d1
MatthewDodi <matthew.dodi@gmail.com>
2019-06-24 21:48:30 -0700

    Added post component that displays the posts programmatically

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,19 +1,35 @@
 import React from "react";
 import { graphql } from "gatsby";
 import { css } from "@emotion/core";
+import { MDXRenderer } from "gatsby-mdx";
 import Layout from "../components/layout";
 import ReadLink from "../components/readLink";
 
-const PostTemplate = () => (
+export const query = graphql`
+  query($slug: String!) {
+    mdx(frontmatter: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        author
+      }
+      code {
+        body
+      }
+    }
+  }
+`;
+
+const PostTemplate = ({ data: { mdx: post } }) => (
   <Layout>
-    <h1>Post title</h1>
+    <h1>{post.frontmatter.title}</h1>
     <p
       css={css`
         font-size: 0.75rem;
       `}
     >
-      Posted by (author)
+      Posted by {post.frontmatter.author}
     </p>
+    <MDXRenderer>{post.code.body}</MDXRenderer>
     <ReadLink to="/">&larr; back to all posts</ReadLink>
   </Layout>
 );
